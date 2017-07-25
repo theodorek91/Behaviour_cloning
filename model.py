06f5d8cd0af1775e57252a314bb1479d1458117a
Theodore King <tking@dspaceinc.us>
2017-07-25 17:48:09 -0400

    Final Commit

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+#import required packages
 import numpy as np
 import keras
 import cv2
@@ -7,33 +8,77 @@ from keras.models import *
 from keras.layers import *
 from keras.callbacks import EarlyStopping as ES #ES(monitor='val_loss', min_delta=0.11, patience=5, verbose=0, mode='max')
 from keras.preprocessing.image import ImageDataGenerator
+import csv
+import matplotlib.pyplot as plt
+import pickle
+from keras.utils import plot_model
+import matplotlib.pyplot as plt
 
-ImageFolderPath = "ot\\IMG\\"
-LogPath = "ot\\driving_log.csv"
+#since AWS instance does not have UI for plot
+plt.switch_backend('agg')
+
+#Required variable Definitions
 batchsize = 50
-nepoch = 10
+nepoch = 50
 
 #Function definitions
 
-#pre-processing : Resize
-def image_process(img):
-	return cv2.resize((cv2.cvtColor(img,cv2.Color_RGB2HSV))[:,:,1],(16,32))
-	#
-	
+def image_func(img):  
+	#pre processing. _ Did not work. Worked for training but on simulator complained about shape of input. 
+	#Was meant to add extra layer to input image of Canny transform so that gradiest are taken into consideration during training to provide better accuracy.
+	img2=cv2.Canny(img,threshold1=200,threshold2=200) 
+	img3=np.concatenate((img,np.reshape(img2,(160,320,1))),axis=2)
+	print(np.asarray(img3).shape)
+	yield img3
+
 #load database
 def load_data():
-	return 0
+	[X_train,Y_train]=pickle.load(open('/home/carnd/CarND/data3.p','rb'))
+
+	#to load data from the training images since large, use pickle file.
+	'''
+	lines=[]
+	with open('/home/carnd/CarND/ot/driving_log.csv') as csvfile:
+		reader = csv.reader(csvfile)
+		for line in reader:
+			lines.append(line)
+	images=[]
+	measurements = []
+	for line in lines:
+		for i in range(3):
+			source_path=line[i]
+			filename=source_path.split('\\')[-1]
+			current_path='/home/carnd/CarND/ot/IMG/'+ filename
+			image=cv2.imread(current_path)
+			images.append(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
+			if i ==0:
+				measurement = float(line[3])
+				measurements.append(measurement)
+			if i ==1:
+				measurement = float(line[3])+0.15
+				measurements.append(measurement)
+			if i==2:
+				measurement = float(line[3]) -0.15
+				measurements.append(measurement)
+	X_train =np.array(images)
+	Y_train = np.array(measurements)
+	'''
+	print(X_train.shape,Y_train.shape)
+	return X_train,Y_train
 	
 
 	
 if __name__ == '__main__':
 	print("loading data")
-	print("Augmentation") #pre-processing : Normalize
-	datagen = ImageDataGenerator(samplewise_std_normalization=True)	
-	datagen.fit(X_train)
+	X_train,Y_train=load_data()
+	
+	#print("Augmentation") #pre-processing : Normalize   
+	#this part didnt work kept giving memory error due to large dataset.
+	#datagen = ImageDataGenerator(samplewise_std_normalization=True)	
+	#datagen.fit(np.asarray(X_train))
 
 	print("splitting data")
-	(XTrain,YTrain,XVal,YVal)=train_test_split(X_train,Y_train,test_size=0.2,random_state=666)
+	(XTrain,XVal,YTrain,YVal)=train_test_split(X_train,Y_train,test_size=0.2,random_state=666)
 	
 	'''
 	Attempt1 - Implementation of modified Alexnet Layer pattern.
@@ -59,11 +104,13 @@ if __name__ == '__main__':
 	Also, Callbacks implemented to monitor Validation accuracy to stop training if value stops increasing.
 
 	'''
-	callbacks = [ES(monitor='val_Accuracy', min_delta=0.11, patience=5, verbose=0, mode='max')]
+	#for Early stopping of model incase loss stopped decreasin in order of 0.001 0r 0.1 percent
+	callbacks = [ES(monitor='loss', min_delta=0.001, patience=5, verbose=1, mode='min')]
 
-
+	
 	model = Sequential()
-	model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+	model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+	model.add(Cropping2D(cropping=((50,20), (0,0))))
 	model.add(Convolution2D(96,11,11,subsample= (4,4),activation='relu'))
 	model.add(ZeroPadding2D())
 	model.add(MaxPooling2D(pool_size=(3,3),strides=(4,4)))
@@ -87,16 +134,25 @@ if __name__ == '__main__':
 	model.add(Dropout(0.5))
 	model.add(Dense(4096, activation='relu'))
 	model.add(Dropout(0.5))
-	model.add(Dense(1000, activation='relu'))
-	model.add(Activation('softmax'))
+	model.add(Dense(1000, activation= 'relu'))
+	model.add(Dense(1))
 	model.summary()
 	
 	print("training model")
-	model.compile(loss='mean_squared_error',optimizer='adam',metrics=['accuracy'])
-	history = model.fit_generator(datagen.flow(XTrain, YTrain, batch_size=batch_size), epochs=nepoch, verbose=1, validation_data=(XVal, YVal), callbacks=callbacks)
-	
+	model.compile(loss='mse',optimizer='adam')
+	history = model.fit(XTrain, YTrain, batch_size=batchsize,epochs=50, verbose=1, callbacks=callbacks, validation_data=(XVal, YVal))
+
+	plot_model(model, to_file='model.png', show_shapes='True')
+	print(history.history['loss'])
+	plt.plot(history.history['loss'])
+	plt.plot(history.history['val_loss'])
+	plt.title('model loss')
+	plt.ylabel('loss')
+	plt.xlabel('epoch')
+	plt.legend(['train','test'],loc='upper left')
+	plt.savefig('lossHistory.png')
 	print("Saving Model")
-	model.save_weights("model.h5")
+	model.save("model.h5")
 	print("model saved")
 	print("   ....    ")
 	print("End of Script")
